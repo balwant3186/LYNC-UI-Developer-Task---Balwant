b317bb5aa3d0b261f048df4e994125995c319947
Balwant <balwantsingh3186@gmail.com>
2024-04-24 20:55:56 +0530

    blockchain gaming section improved

--- components/blockchain-gaming/blockchain-gaming.tsx
+++ components/blockchain-gaming/blockchain-gaming.tsx
@@ -3,47 +3,56 @@ import "./blockchain-gaming.scss";
 
 const BlockchainGaming = () => {
   return (
-    <section className="blockchain-gaming-section flex column items-center">
-      <h3 className="blockchain-gaming-title text-display-2 text-center">
-        Making blockchain gaming accessible to{" "}
-        <span className="gradient-text gradient-secondary">everyone!</span>
-      </h3>
-      <div className="blockchain-gaming-img-container text-center">
-        <Image
-          src="/assets/zora-verse.png"
-          alt="zora-verse"
-          className="blockchain-gaming-img"
-          width={200}
-          height={150}
-        />
-        <Image
-          src="/assets/born-to-die.png"
-          alt="born-to-die"
-          className="blockchain-gaming-img"
-          width={200}
-          height={150}
-        />
-        <Image
-          src="/assets/souls-of-meta.png"
-          alt="souls-of-meta"
-          className="blockchain-gaming-img"
-          width={200}
-          height={150}
-        />
-        <Image
-          src="/assets/lumos.png"
-          alt="lumos"
-          className="blockchain-gaming-img"
-          width={200}
-          height={150}
-        />
-        <Image
-          src="/assets/vr.png"
-          alt="vr"
-          className="blockchain-gaming-img"
-          width={200}
-          height={150}
-        />
+    <section className="blockchain-gaming-container relative flex column items-center justify-center">
+      <div className="blockchain-gaming flex column items-center">
+        <h2 className="blockchain-gaming-title text-display-2 text-center">
+          Making blockchain gaming accessible to{" "}
+          <span className="gradient-text gradient-secondary">everyone!</span>
+        </h2>
+        <div className="gaming-imgs-container flex items-center justify-center wrap">
+          <Image
+            src="/assets/zuraverse.png"
+            alt="zuraverse"
+            className="gaming-imgs"
+            width={190}
+            height={50}
+          />
+          <Image
+            src="/assets/born-to-die.png"
+            alt="born to die"
+            className="gaming-imgs"
+            width={190}
+            height={50}
+          />
+          <Image
+            src="/assets/souls-of-meta.png"
+            alt="souls of meta"
+            className="gaming-imgs"
+            width={190}
+            height={50}
+          />
+          <Image
+            src="/assets/lumos.png"
+            alt="lumos"
+            className="gaming-imgs"
+            width={190}
+            height={50}
+          />
+          <Image
+            src="/assets/cybersyndicate.png"
+            alt="cyber syndicate"
+            className="gaming-imgs"
+            width={190}
+            height={50}
+          />
+          <Image
+            src="/assets/metaborn.svg"
+            alt="meta born"
+            className="gaming-imgs"
+            width={190}
+            height={50}
+          />
+        </div>
       </div>
     </section>
   );
