2b84a341963e7e13887e636db0213630fee9bf44
Balwant <balwantsingh3186@gmail.com>
2024-04-25 18:01:42 +0530

    community and chains sections minor fixes

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ import ValueableReviews from "@/components/valuable-reviews/valuable-reviews";
 export default function Home() {
   return (
     <div>
-      <Header />
+      {/* <Header />
       <Hero />
 
       <BlockchainGaming />
@@ -24,15 +24,15 @@ export default function Home() {
 
       <ProductSuite />
 
-      {/* <Chains />
+      <Chains /> */}
 
-      <Blog />
+      {/* <Blog /> */}
 
-      <ValueableReviews /> */}
+      <ValueableReviews />
 
-      {/* <Community /> */}
+      {/* <Community />
 
-      {/* <Believers />
+      <Believers />
 
       <Footer /> */}
     </div>
